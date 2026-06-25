6a9a822e22f74bc6ed790624b66a389f4e356c2e
erikcfrancisco.bautista <erikcfrancisco.bautista@gmail.com>
2026-06-24 18:01:50 -0600

    [BBOM-MODULE-#1] : refact implementation

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,11 @@ export { loginRoutes };
 export { buildLoginUseCase } from "./services/login.implementation.js";
 export type { LoginAdapters } from "./contracts.js";
 export type { LoginControllerAdapters } from "./contracts.js";
+export type { LoginRoutesPluginOptions } from "./contracts.js";
 export type { LoginOutput } from "./models/dto/output/login.dto.js";
 export type { LoginInput } from "./models/dto/input/login.dto.js";
+export { configureModuleLogger } from "./lib/module-logger.js";
+export type { ModuleLoggerAdapter } from "./lib/module-logger.js";
 export {
   LoginError,
   InvalidCredentialsError,
